5623339c511d7defcb94b8b72104804a07b098e1
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2022-03-09 21:18:05 -0500

    Initial commit

---
+++ Assets/js/functions.js
@@ -0,0 +1,37 @@
+const url = ("http://localhost:3000/NEAr/")
+let campo = document.querySelector("#socialcard")
+//  
+var contador = 0;
+let msg = "Solo puedes dar un clic por obra de arte"
+function contar() {
+    console.log("entra")
+    document.getElementById("btnlikes").innerHTML = contador++;
+    if (contador > 1) {
+        contador = 1;
+        alert(msg)
+    }
+
+}
+const getData = async (url) => {
+
+    const resp = await fetch(url);
+    
+    console.log(resp)
+    const data = await resp.json();
+    data.forEach(card => {
+        alert("ENTROOOOO")
+        const { nombre, foto, coleccion, precio, likes, nickname } = card;
+        campo.innerHTML += ` 
+        <div class="card">
+        <img src="${foto}" alt="">
+        <p>${nombre}</p>
+        <p style="color:blue; font-weigth:bold">${nickname}</p>
+        <p>${precio}</p>
+        <p>"Colección: "${coleccion}</p>
+        <button id="btnlikes" class="btn btn-primary" onclick="contar()">${likes} Like </button>
+        `
+
+    })
+}
+
+window.addEventListener('DOMContentLoaded', getData);
